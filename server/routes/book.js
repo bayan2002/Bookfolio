8767a51954671fd636c7dd6c95459a094c0f2b6b
bayan2002 <bayanabdelbaryq@gmail.com>
2023-08-12 11:36:34 +0300

    handle book routes #5

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const bookRouter = express.Router();
 
+const {
+  addBook,
+  getAllUserBooks,
+  getSingleBook,
+  updateBook,
+  deleteBook,
+  searchBooks,
+  getStarredBooks,
+} = require("../controllers/book");
+const verifyToken = require("../middlewares/verifyToken");
+const errorCatcher = require("../middlewares/errorCatcher");
 
-// post => '/'
-// get => '/'
-// get => '/search'
-// get => '/starred'
-// get => '/:bookId'
-// put => '/:bookId'
-// delete => '/:bookId'
+bookRouter.post("/", verifyToken, errorCatcher(addBook));
 
+bookRouter.get("/", verifyToken, errorCatcher(getAllUserBooks));
+
+bookRouter.get("/search", verifyToken, errorCatcher(searchBooks));
+
+bookRouter.get("/starred", verifyToken, errorCatcher(getStarredBooks));
+
+bookRouter.get("/:bookId", verifyToken, errorCatcher(getSingleBook));
+
+bookRouter.put("/:bookId", verifyToken, errorCatcher(updateBook));
+
+bookRouter.delete("/:bookId", verifyToken, errorCatcher(deleteBook));
 
 module.exports = bookRouter;
