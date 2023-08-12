53a165c5774090289c55c9570e100f926af3a718
bayan2002 <bayanabdelbaryq@gmail.com>
2023-08-12 10:41:00 +0300

    app.js setup & install server pakages #3

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const compression = require("compression");
 const router = require("./routes");
+
+dotenv.config();
 
 const app = express();
 
-app.set("port", 5000);
+app.set("port", process.env.PORT || 5000);
+app.use(cors());
+
+app.use([
+  express.json(),
+  compression(),
+  express.urlencoded({ extended: false }),
+]);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // app.use("/api/v1", router);
+
 
 module.exports = app;
