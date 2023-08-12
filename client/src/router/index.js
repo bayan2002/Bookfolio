8afcb677077d0b83d01f6ba8673158a36f434a41
bayan2002 <bayanabdelbaryq@gmail.com>
2023-08-12 13:21:00 +0300

    handle router #15

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "auth",
+      component: () => import("../views/AuthView.vue"),
+      children: [
+        {
+          // when / is matched
+          path: "",
+          name: "login",
+          component: () => import("../pages/Login.vue"),
+        },
+        {
+          // when /register is matched
+          path: "register",
+          name: "register",
+          component: () => import("../pages/Register.vue"),
+        },
+      ],
+    },
   ]
 })
 
